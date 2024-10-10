eb911376e41f761484afce47f390b11ded49ad9b
Radium Zhang <radium@lawn-128-61-2-110.lawn.gatech.edu>
2024-10-10 16:13:07 -0400

    fix(sensors): modify the sensor code

--- sensors.py
+++ sensors.py
@@ -7,7 +7,10 @@ PIR_PIN = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
+print("Waiting for the PIR sensor to initialize...")
+
 
 def user_interaction_detected():
     """Detect if motion is detected and hand is placed on the statue's mouth."""
-    return GPIO.input(PIR_PIN)
+    print("Checking for user interaction...")
+    return GPIO.input(PIR_PIN) == GPIO.HIGH
